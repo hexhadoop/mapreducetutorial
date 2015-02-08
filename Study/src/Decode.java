4f1d6352248e1d74327210a1460d00f4f138f2a5
rita maa <rita maa@rita-1b3a8c4e75>
2015-02-08 16:24:39 +0530

    TESTING

--- Study/src/Decode.java
+++ Study/src/Decode.java
@@ -6,6 +6,7 @@ public class Decode {
 	public static void main(String... ar)throws Exception{
 		FileInputStream fileIn = new FileInputStream("C:/Documents and Settings/rita maa/Desktop/test.ser");
 		ObjectInputStream in = new ObjectInputStream(fileIn);
+		
 		String a = (String) in.readObject(); 
 		System.out.println(a);
 	}
